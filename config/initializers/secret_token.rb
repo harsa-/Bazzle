ace8b090da3699a750903cb8e07610b430d9a357
Harri Sarsa <harri.sarsa@gmail.com>
2011-04-07 14:29:40 +0300

    Initial Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bazzle::Application.config.secret_token = '7ee9d70e4747b77c3b6802b412df5a0e74bb826e107d06cd2e2537f91ca3c37deac3cef8f12adb9514b058c902bdc624b677739fd7c131bf367ded7edd0dfc71'
